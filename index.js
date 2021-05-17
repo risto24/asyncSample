e761ae4394196205c3d547ed856b5c8e655a5c9d
Risto Nishimura <risto24.contact@gmail.com>
2021-05-17 02:42:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const sampleFunc1 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      return resolve('1');
+    }, Math.random() * 3000);
+  });
+}
+
+const sampleFunc2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      return resolve('2');
+    }, Math.random() * 3000);
+  });
+}
+
+(async () => {
+  await sampleFunc1().then(async(res) => {
+    console.log(res);
+    await sampleFunc2().then((res) => {
+      console.log(res);
+    }); 
+    console.log('Done');
+  });
+})();
